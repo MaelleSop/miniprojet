43f59ce3c1f68392c437dfefa70852e0ff43dd3d
Cassie <131532959+Cassuue@users.noreply.github.com>
2023-06-11 19:36:26 +0200

    Update Lancement.cpp

--- src/Lancement.cpp
+++ src/Lancement.cpp
@@ -9,63 +9,32 @@
 
 Lancement::Lancement(QWidget* parent) : QMainWindow(parent){
     this->resize(500,840);
-
+    this->setWindowTitle("Bienvenue !");
+    
     QWidget* mainWidget = new QWidget();
     QVBoxLayout* layout = new QVBoxLayout();
 
     this->bouton = new QPushButton("Play");
-    //QLabel* pseudo = new QLabel("Entrer un pseudo :");
+    QLabel* pseudo = new QLabel("Entrer un pseudo :");
     this->lineEdit = new QLineEdit();
 
-    layout->addWidget(bouton);
+    layout->addWidget(pseudo);
     layout->addWidget(lineEdit);
-    //layout->addWidget(play);
+    layout->addWidget(bouton);
 
     connect(bouton, SIGNAL(clicked()), this, SLOT(recupPseudoPlay()));
 
     mainWidget->setLayout(layout);
     this->setCentralWidget(mainWidget);
 
-    /*QVBoxLayout* layout = new QVBoxLayout();
-    QWidget* widget = new QWidget;
-    widget->setLayout(layout);
-
-    QGraphicsProxyWidget *proxyWidget = new QGraphicsProxyWidget();
-
-    QPushButton* play = new QPushButton("Play");
-    QLabel* pseudo = new QLabel("Entrer un pseudo :");
-    this->inputPseudo = new QLineEdit();
-
-    layout->addWidget(pseudo);
-    layout->addWidget(inputPseudo);
-    layout->addWidget(play);
-
-    proxyWidget->setWidget(widget);
-
-    // Calcul de la position pour centrer le widget
-    float centerX = 500 / 2 - proxyWidget->rect().width() / 2;
-    float centerY = 800 / 2 - proxyWidget->rect().height() / 2;
-    proxyWidget->setPos(centerX, centerY);
-
-    addItem(proxyWidget);
-
-    connect(play, SIGNAL(clicked()), this, SLOT(recupPseudoPlay()));*/
 }
-
-/*void Lancement::drawBackground(QPainter* painter, const QRectF &rect) {
-    Q_UNUSED(rect);
-    QPixmap pixBackground("../img/foret_test.jpg");
-    painter->drawPixmap(QPointF(0,0), pixBackground, sceneRect());
-}*/
 
 void Lancement::recupPseudoPlay() {
     QString Qpseudo = lineEdit->text();
-    string pseudo = Qpseudo.toStdString();
 
     close();
 
-    MainWindow* mainWindow = new MainWindow(this);
+    MainWindow* mainWindow = new MainWindow(Qpseudo, this);
     mainWindow->show();
 
-    qDebug() << Qpseudo;
 }
